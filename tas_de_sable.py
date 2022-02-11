428096f26593bf6cbf6a570123b6cd223980428a
Lucas AUCLAIR <lucas.auclair@ens.uvsq.fr>
2022-02-11 10:28:04 +0100

    Nom du groupe ajouté

---
+++ tas_de_sable.py
@@ -0,0 +1,7 @@
+#####################################################
+# groupe LDDBI L1
+# Lucas AUCLAIR
+# Nikita VERSHYNIN
+# Camille LE CORRE
+# https://github.com/uvsq22102239/projet_tas_de_sable
+#####################################################
