affade15d39bde460dbe227f631ec9acb321727b
Camille LE CORRE <camille.le-corre@ens.uvsq.fr>
2022-02-11 20:13:48 +0100

    Camille - 11.02

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -28,6 +28,8 @@ import tkinter as tk
 hauteur_canevas = 500
 largeur_canevas = 500
 
+nb_cases_grille = 100
+
 
 
 #####################################################
@@ -43,9 +45,33 @@ racine.title("Ecoulement d'un tas de sable")
 # Création des widgets
 #####################################################
 
+##### Canevas
 canevas = tk.Canvas(racine, height=hauteur_canevas, width=largeur_canevas, bg="white")
 
+
+def creationGrille(nb_cases):
+    """ Fonction créant la grille affichée sur le canevas, qui prend en argument le nombre
+        de cases contenues dans une ligne ou une colonne (car la grille est carrée)"""
+
+
+    taille_case = largeur_canevas // nb_cases
+
+    for i in range(1, (nb_cases)):
+        # lignes verticales
+        canevas.create_line((i * taille_case), 0, (i * taille_case), hauteur_canevas)
+        # lignes horizontales
+        canevas.create_line(0, (i * taille_case), largeur_canevas, (i * taille_case))
+
+    return
+
+
+creationGrille(nb_cases_grille)
+
+
+##### Boutons
 bouton_configuration_aleatoire = tk.Label(racine, text="Configuration aléatoire", bg="grey")
+
+##### Cases grille
 
 
 #####################################################
