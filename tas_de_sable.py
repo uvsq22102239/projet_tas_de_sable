7636082469c9071d48c627392471ca3167fbe151
Camille LE CORRE <camille.le-corre@ens.uvsq.fr>
2022-02-11 20:49:02 +0100

    Camille - 11.02 (restructuration)

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -22,31 +22,24 @@ import tkinter as tk
 
 
 #####################################################
-# Définition des variables
+# Définition des constantes
 #####################################################
 
-hauteur_canevas = 500
-largeur_canevas = 500
+HAUTEUR_CANEVAS = 500
+LARGEUR_CANEVAS = 500
 
-nb_cases_grille = 100
-
+NB_CASES_GRILLE = 100
 
 
 #####################################################
-# Boucle principale
+# Définition des variables globales
 #####################################################
-
-racine = tk.Tk()
-racine.title("Ecoulement d'un tas de sable")
 
 
 
 #####################################################
-# Création des widgets
+# Définition des fonctions
 #####################################################
-
-##### Canevas
-canevas = tk.Canvas(racine, height=hauteur_canevas, width=largeur_canevas, bg="white")
 
 
 def creationGrille(nb_cases):
@@ -54,29 +47,47 @@ def creationGrille(nb_cases):
         de cases contenues dans une ligne ou une colonne (car la grille est carrée)"""
 
 
-    taille_case = largeur_canevas // nb_cases
+    taille_case = LARGEUR_CANEVAS // nb_cases
 
     for i in range(1, (nb_cases)):
         # lignes verticales
-        canevas.create_line((i * taille_case), 0, (i * taille_case), hauteur_canevas)
+        canevas.create_line((i * taille_case), 0, (i * taille_case), HAUTEUR_CANEVAS)
         # lignes horizontales
-        canevas.create_line(0, (i * taille_case), largeur_canevas, (i * taille_case))
+        canevas.create_line(0, (i * taille_case), LARGEUR_CANEVAS, (i * taille_case))
 
     return
 
 
-creationGrille(nb_cases_grille)
+#####################################################
+# Boucle principale
+#####################################################
+
+racine = tk.Tk()
+racine.title("Simulation de l'écoulement d'un tas de sable")
+
+
+
+#####################################################
+# Création des widgets
+
+
+##### Canevas
+canevas = tk.Canvas(racine, height=HAUTEUR_CANEVAS, width=LARGEUR_CANEVAS, bg="white")
+
+creationGrille(NB_CASES_GRILLE)
 
 
 ##### Boutons
 bouton_configuration_aleatoire = tk.Label(racine, text="Configuration aléatoire", bg="grey")
 
+
 ##### Cases grille
+
 
 
 #####################################################
 # Placement des widgets
-#####################################################
+
 
 canevas.grid(column=1, row=0)
 
@@ -84,8 +95,8 @@ bouton_configuration_aleatoire.grid(column=0, row=0)
 
 
 #####################################################
-# Fonctions liées aux widgets (commandes des widgets)
-#####################################################
+# Gestion des évenements liés aux widgets
+
 
 
 
