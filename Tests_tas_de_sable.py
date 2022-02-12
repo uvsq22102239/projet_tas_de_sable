920043de237398aa8dd441a8790a5ffcbaef081b
Camille LE CORRE <camille.le-corre@ens.uvsq.fr>
2022-02-12 07:54:04 +0100

    Camille - 12.02 (création cases de la grille)

--- Tests_tas_de_sable.py
+++ Tests_tas_de_sable.py
@@ -1,4 +1,19 @@
 #####################################################
+# groupe LDDBI L1
+# Lucas AUCLAIR
+# Nikita VERSHYNIN
+# Camille LE CORRE
+# https://github.com/uvsq22102239/projet_tas_de_sable
+#####################################################
+
+
+
+
+
+
+
+
+#####################################################
 # Import des librairies
 #####################################################
 
@@ -7,12 +22,40 @@ import tkinter as tk
 
 
 #####################################################
-# Définition des variables
+# Définition des constantes
 #####################################################
 
-hauteur_canevas = 500
-largeur_canevas = 500
+HAUTEUR_CANEVAS = 500
+LARGEUR_CANEVAS = 500
 
+NB_CASES_GRILLE = 100
+
+
+#####################################################
+# Définition des variables globales
+#####################################################
+
+
+#####################################################
+# Définition des fonctions
+#####################################################
+
+
+def creationGrille(nb_cases):
+    """ Fonction créant la grille affichée sur le canevas, qui prend en argument le nombre
+        de cases contenues dans une ligne ou une colonne (car la grille est carrée)"""
+
+
+    taille_case = LARGEUR_CANEVAS // nb_cases
+
+
+    for k in range(1, (nb_cases)):
+        # lignes verticales
+        canevas.create_line((k * taille_case), 0, (k * taille_case), HAUTEUR_CANEVAS)
+        # lignes horizontales
+        canevas.create_line(0, (k * taille_case), LARGEUR_CANEVAS, (k * taille_case))
+
+    return
 
 
 #####################################################
@@ -20,22 +63,31 @@ largeur_canevas = 500
 #####################################################
 
 racine = tk.Tk()
-racine.title("Ecoulement d'un tas de sable")
+racine.title("Simulation de l'écoulement d'un tas de sable")
 
 
 
 #####################################################
 # Création des widgets
-#####################################################
 
-canevas = tk.Canvas(racine, height=hauteur_canevas, width=largeur_canevas, bg="white")
 
+##### Canevas
+canevas = tk.Canvas(racine, height=HAUTEUR_CANEVAS, width=LARGEUR_CANEVAS, bg="white")
+
+creationGrille(NB_CASES_GRILLE)
+
+
+##### Boutons
 bouton_configuration_aleatoire = tk.Label(racine, text="Configuration aléatoire", bg="grey")
+
+
+##### Cases grille
+
 
 
 #####################################################
 # Placement des widgets
-#####################################################
+
 
 canevas.grid(column=1, row=0)
 
@@ -43,11 +95,37 @@ bouton_configuration_aleatoire.grid(column=0, row=0)
 
 
 #####################################################
-# Fonctions liées aux widgets (commandes des widgets)
-#####################################################
+# Gestion des évenements liés aux widgets
 
 
 
+
+def coordonneesCase(ligne, colonne):
+    """ Définie les coordonnées d'une case de la grille """
+
+    taille_case = LARGEUR_CANEVAS // NB_CASES_GRILLE
+    
+    x0 = colonne * taille_case
+    y0 = ligne * taille_case
+    x1 = (colonne + 1) * taille_case
+    y1 = (ligne + 1) * taille_case
+
+    liste_coordonnées = [x0, y0, x1, y1]
+
+    return liste_coordonnées
+
+
+def creationCase(ligne, colonne):
+    """ Créer un carré correspondant à une case en fonction de son placement dans la grille """
+    
+    canevas.create_rectangle(coordonneesCase(ligne, colonne), fill="red")
+
+    return
+
+
+for i in range(NB_CASES_GRILLE):
+    for j in range(NB_CASES_GRILLE):
+        creationCase(i, j)
 
 
 
--- tas_de_sable.py
+++ tas_de_sable.py
@@ -49,11 +49,38 @@ def creationGrille(nb_cases):
 
     taille_case = LARGEUR_CANEVAS // nb_cases
 
-    for i in range(1, (nb_cases)):
+    for k in range(1, (nb_cases)):
         # lignes verticales
-        canevas.create_line((i * taille_case), 0, (i * taille_case), HAUTEUR_CANEVAS)
+        canevas.create_line((k * taille_case), 0, (k * taille_case), HAUTEUR_CANEVAS)
         # lignes horizontales
-        canevas.create_line(0, (i * taille_case), LARGEUR_CANEVAS, (i * taille_case))
+        canevas.create_line(0, (k * taille_case), LARGEUR_CANEVAS, (k * taille_case))
+
+    return
+
+
+
+
+def coordonneesCase(ligne, colonne):
+    """ Définie les coordonnées d'une case de la grille """
+
+    taille_case = LARGEUR_CANEVAS // NB_CASES_GRILLE
+    
+    x0 = colonne * taille_case
+    y0 = ligne * taille_case
+    x1 = (colonne + 1) * taille_case
+    y1 = (ligne + 1) * taille_case
+
+    liste_coordonnées = [x0, y0, x1, y1]
+
+    return liste_coordonnées
+
+
+
+
+def creationCase(ligne, colonne):
+    """ Créer un carré correspondant à une case en fonction de son placement dans la grille """
+    
+    canevas.create_rectangle(coordonneesCase(ligne, colonne), fill="red")
 
     return
 
@@ -66,15 +93,20 @@ racine = tk.Tk()
 racine.title("Simulation de l'écoulement d'un tas de sable")
 
 
-
 #####################################################
 # Création des widgets
 
 
-##### Canevas
+##### Canevas (+ grille + cases)
 canevas = tk.Canvas(racine, height=HAUTEUR_CANEVAS, width=LARGEUR_CANEVAS, bg="white")
 
 creationGrille(NB_CASES_GRILLE)
+
+for i in range(NB_CASES_GRILLE):
+    for j in range(NB_CASES_GRILLE):
+        creationCase(i, j)
+
+
 
 
 ##### Boutons
