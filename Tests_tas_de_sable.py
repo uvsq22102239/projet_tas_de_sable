f2c9467b3528706c67f59295b8182ea89f5f2e8e
Camille LE CORRE <camille.le-corre@ens.uvsq.fr>
2022-02-16 15:01:28 +0100

    Camille - 16.02 (création + initialisation configuration + couleurs)

--- Tests_tas_de_sable.py
+++ Tests_tas_de_sable.py
@@ -35,6 +35,9 @@ NB_CASES_GRILLE = 100
 # Définition des variables globales
 #####################################################
 
+global case
+
+global configuration_courante
 
 #####################################################
 # Définition des fonctions
@@ -80,6 +83,7 @@ creationGrille(NB_CASES_GRILLE)
 ##### Boutons
 bouton_configuration_aleatoire = tk.Label(racine, text="Configuration aléatoire", bg="grey")
 
+# /!\ Pas Label mais Button
 
 ##### Cases grille
 
@@ -118,17 +122,82 @@ def coordonneesCase(ligne, colonne):
 def creationCase(ligne, colonne):
     """ Créer un carré correspondant à une case en fonction de son placement dans la grille """
     
-    canevas.create_rectangle(coordonneesCase(ligne, colonne), fill="red")
+    case = canevas.create_rectangle(coordonneesCase(ligne, colonne), fill="saddle brown")
 
     return
 
 
-for i in range(NB_CASES_GRILLE):
-    for j in range(NB_CASES_GRILLE):
-        creationCase(i, j)
+#for i in range(NB_CASES_GRILLE):
+    #for j in range(NB_CASES_GRILLE):
+        #creationCase(i, j)
+
+###############################
+
 
 
 
 
 
 racine.mainloop()
+
+
+
+##### Couleurs cases en fonction nombre grains de sable :
+# 0 => "white"
+# 1 => "blanched almond"
+# 2 => "bisque2"
+# 3 => "Darkgoldenrod3"
+# 4 => "tan4"
+# 5 => "sienna4"
+# 6 => "saddle brown"
+
+
+def creationConfiguration(taille):
+    """ Créer une matrice carrée nulle de taille choisie par l'utilisateur"""
+
+    ligne = []
+    matrice = []
+
+    for i in range(taille):
+        ligne.append(0)
+
+    for j in range(taille):
+        matrice.append(ligne)
+
+    return matrice
+
+
+
+
+def initialisationConfiguration(matrice):
+    """ Chaque élément de la matrice est remplacé par un zéro : on obtient une matrice nulle"""
+
+    for i in range(len(matrice)):
+        for j in range(len(matrice)):
+            matrice[i][j] = 0
+
+
+
+
+def couleurCases(matrice):
+    """ Colorie toutes les cases en fonction de leur nombre de grain de sable"""
+
+
+    for i in range(NB_CASES_GRILLE):
+        for j in range(NB_CASES_GRILLE):
+            if matrice[i][j] == 0:
+                canevas.itemconfigure(case, fill="white")
+            elif matrice[i][j] == 1:
+                canevas.itemconfigure(case, fill="blanched almond")
+            elif matrice[i][j] == 2:
+                canevas.itemconfigure(case, fill="bisque2")
+            elif matrice[i][j] == 3:
+                canevas.itemconfigure(case, fill="Darkgoldenrod3")
+            elif matrice[i][j] == 4:
+                canevas.itemconfigure(case, fill="tan4")
+            elif matrice[i][j] == 5:
+                canevas.itemconfigure(case, fill="sienna4")
+            elif matrice[i][j] == 6:
+                canevas.itemconfigure(case, fill="saddle brown")
+            else :
+                canevas.itemconfigure(case, fill="black")
